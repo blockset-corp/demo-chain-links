cc157ec369678b546525d22e3badba8a5aa26a46
Michael Carrara <michael.carrara@breadwallet.com>
2021-08-06 16:46:39 -0400

    Drop Singleton use for height checks

--- chainlinks/tasks.py
+++ chainlinks/tasks.py
@@ -16,16 +16,19 @@ check_single_engine = ChainCheckEngine(signature('chainlinks.tasks.run_check_hei
 # Tasks
 
 
-@shared_task(base=Singleton, lock_expiry=60, ignore_result=True)
+CHAIN_CHECK_ALL_EXPIRY=60
+
+
+@shared_task(base=Singleton, ignore_result=True, expiry=CHAIN_CHECK_ALL_EXPIRY, lock_expiry=CHAIN_CHECK_ALL_EXPIRY)
 def run_all_check_jobs():
     check_all_engine.check_all_chains()
 
 
-@shared_task(base=Singleton, lock_expiry=CHAIN_CHECK_EXPIRY, ignore_result=True)
+@shared_task(base=Singleton, ignore_result=True, expiry=CHAIN_CHECK_EXPIRY, lock_expiry=CHAIN_CHECK_EXPIRY)
 def run_check_job(job_pk: Any):
     check_single_engine.check_chain(job_pk)
 
 
-@shared_task(base=Singleton, lock_expiry=CHAIN_CHECK_EXPIRY, ignore_result=True, queue='consumer')
+@shared_task(queue='consumer', ignore_result=True, expiry=CHAIN_CHECK_EXPIRY)
 def run_check_height(job_pk: Any, result_pk: Any, blockchain_id: str, block_height: int, service_id: str):
     check_single_engine.check_block(job_pk, result_pk, blockchain_id, block_height, service_id)
